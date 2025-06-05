6189efca6d98d76acd8bd135669cc34be43c028a
Anjani Kumar <anjan.1204kr@gmail.com>
2025-06-05 12:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // prevent form from refreshing page
 
+  const firstName = document.querySelector('input[name="fname"]').value.trim();
+  const lastName = document.querySelector('input[name="lname"]').value.trim();
+
+  const fullName = `${firstName} ${lastName}`.trim();
+
+  if (!firstName && !lastName) {
+    alert("Please enter your first and last name.");
+  } else {
+    alert(fullName);
+  }
 }
